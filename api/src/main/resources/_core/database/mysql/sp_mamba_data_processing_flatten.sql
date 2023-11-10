52577b4d070c9b76a03573ddaaf0311a78adcc49
CynthiaKamau <cynthiakamau54@gmail.com>
2023-11-10 22:11:47 +0300

    Add db configurations

--- api/src/main/resources/_core/database/mysql/sp_mamba_data_processing_flatten.sql
+++ api/src/main/resources/_core/database/mysql/sp_mamba_data_processing_flatten.sql
@@ -44,7 +44,7 @@ CALL sp_mamba_flat_encounter_table_create_all;
 
 CALL sp_mamba_flat_encounter_table_insert_all;
 
-CALL sp_mamba_flat_encounter_obs_group_table_create_all;
+-- CALL sp_mamba_flat_encounter_obs_group_table_create_all;
 
-CALL sp_mamba_flat_encounter_obs_group_table_insert_all;
+-- CALL sp_mamba_flat_encounter_obs_group_table_insert_all;
 -- $END
